52e7210d3687f03645f516644f9a1f72c64e5273
T-Terra <gabst138@gmail.com>
2024-09-23 16:39:42 -0300

    feat: add drawer navigation

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,21 @@
-import { enableScreens } from 'react-native-screens'
 import React from 'react'
 import { SafeAreaView } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 
 //import Stack from './Stack'
-import Tab from './Tab'
-
-enableScreens()
+//import Tab from './Tab'
+import Drawer from './Drawer'
 
 export default (props: any) => {
     return (
         <SafeAreaView style={{flex: 1}}>
             <NavigationContainer>
-                <Tab />
-                {/*<Stack />*/}
+                <Drawer />
+                {/*
+                    <Drawer />
+                    <Stack />
+                    <Tab />
+                */}
             </NavigationContainer>
         </SafeAreaView>
     )
