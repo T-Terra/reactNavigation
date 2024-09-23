a4f1d149adbf6cba54a04f9e33c8e8f8befd24ba
T-Terra <gabst138@gmail.com>
2024-09-23 13:19:28 -0300

    feat: add func navigate

--- src/routes/Stack.tsx
+++ src/routes/Stack.tsx
@@ -3,6 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack'
 import TelaA from '../views/TelaA'
 import TelaB from '../views/TelaB'
 import TelaC from '../views/TelaC'
+import PassoStack from '../components/PassoStack'
 
 const Stack = createStackNavigator()
 
@@ -11,9 +12,21 @@ export default (props: any) => {
         <Stack.Navigator initialRouteName="TelaA" screenOptions={{ headerShown: true }}>
             <Stack.Screen name="TelaA" 
                 options={{ title: "Info Iniciais" }}
-                component={TelaA} />
-            <Stack.Screen name="TelaB" component={TelaB} />
-            <Stack.Screen name="TelaC" component={TelaC} />
+            >
+                {(props: any) => (
+                    <PassoStack {...props} avancar="TelaB">
+                        <TelaA/>
+                    </PassoStack>
+                )}
+            </Stack.Screen>
+            <Stack.Screen name="TelaB" >
+                {(props: any) => (
+                    <PassoStack {...props} avancar="TelaC">
+                        <TelaB/>
+                    </PassoStack>
+                )}
+            </Stack.Screen>
+            <Stack.Screen name="TelaC" component={TelaC} ></Stack.Screen>
         </Stack.Navigator>
     )
 }
